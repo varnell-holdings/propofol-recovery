610f86a445b1e34708ae042264060ace1a323d96
John Tillett <tillett1957@gmail.com>
2016-05-08 11:42:09 +1000

    add patient/control field to data.csv

--- csv_setup.py
+++ csv_setup.py
@@ -1,12 +1,14 @@
 import csv
 
-prop_headers = ['identifier', 'sex', 'age', 'weight(kg)', 'height(cm)',
+prop_headers = ['patient/control', 'identifier', 'sex', 'age', 'weight', 'height',
  'alcohol', 'ihd', 'hypertension', 'sleep apnoea', 'diabetes',
-  'cvd', 'dementia','asa', 'procedure(p,c,d)', 'start', 'finish',
-   'last dose time', 'total dose(mg)', 'n/v', 'pain', 'tolerating',
+  'cvd', 'dementia','asa', 'procedure', 'start', 'finish',
+   'last dose time', 'total dose', 'n/v', 'pain', 'tolerating',
      'ready for discharge', 'aldrete', 'time discharge assessment']
 
 def go():
-    with open('propofol_data.csv', 'ab') as csvfile:
+    with open('data.csv', 'ab') as csvfile:
         datawriter = csv.writer(csvfile, dialect='excel')
         datawriter.writerow(prop_headers)
+if __name__ == '__main__':
+    go()
--- enter_data.py
+++ enter_data.py
@@ -2,27 +2,29 @@ import csv, re, click
 
 # using 2.7 in conda env 'propofol'
 
-propofol_titles = [('identifier',r'^\S+$'), ('sex',r'^[mf]$'), ('age', r'^\d{2}$'), ('weight(kg)', r'^\d{2,3}$'), ('height(cm)', r'^\d{3}$'),
+prompts = [('patient/control', r'^[pc]$'),('identifier',r'^\S+$'), ('sex',r'^[mf]$'), ('age', r'^\d{2}$'), ('weight(kg)', r'^\d{2,3}$'), ('height(cm)', r'^\d{3}$'),
  ('alcohol', r'^\d{1,2}$'), ('ihd', r'^[yn]$'), ('hypertension', r'^[yn]$'), ('sleep apnoea',r'^[yn]$'), ('diabetes', r'^[yn]$'),
-  ('cvd', r'^[yn]$'), ('dementia', r'^[yn]$'), ('asa', r'^[1234]$'), ('procedure(p,c,d)', r'^[pcd]$'), ('start', r'^\d\d:\d\d$'), ('finish', r'^\d\d:\d\d$'),
-   ('last dose time', r'^\d\d:\d\d$'), ('total dose(mg)', r'^\d{2,4}$'), ('n/v', r'^[yn]$'), ('pain', r'^[yn]$'), ('tolerating', r'^[yn]$'),
-     ('ready for discharge', r'^[yn]$'), ('aldrete', r'^\d{1,2}$'),('time discharge assessment', r'^\d\d:\d\d$')]
+  ('cvd', r'^[yn]$'), ('dementia', r'^[yn]$'), ('asa', r'^[1234]$'), ('procedure(p,c,d)', r'^[pcd]$'), ('start (hh:mm)', r'^\d\d:\d\d$'), ('finish', r'^\d\d:\d\d$'),
+   ('last dose time', r'^\d\d:\d\d$'), ('total dose(mg)', r'^\d{2,4}$'), ('n/v y/n', r'^[yn]$'), ('pain y/n', r'^[yn]$'), ('tolerating y/n', r'^[yn]$'),
+     ('ready for discharge y/n', r'^[yn]$'), ('aldrete', r'^\d{1,2}$'),('time discharge assessment', r'^\d\d:\d\d$')]
 
 def entry():
     with open('propofol_data.csv', 'ab') as csvfile:
         datawriter = csv.writer(csvfile, dialect='excel')
         while True:
             episode_data = []
-            for data in propofol_titles:
+            for data in prompts:
                 while True:
                     reply = raw_input(data[0] +':  ')
                     if re.search(data[1], reply):
                         episode_data.append(reply)
                         break
                     print '****** ?error ******'
-            assert len(episode_data) ==  len(propofol_titles), 'There seems to be an error. ? missing data'
+            assert len(episode_data) ==  len(prompts), 'There seems to be an error. ? missing data'
             datawriter.writerow(episode_data)
             response = raw_input('Add another episode? (y/n):  ')
             if response == 'n':
-                print 'Exiting. Thanks!!'
+                print 'Exiting. Thanks!! *** Remember to git push origin master and upload to google **'
                 break
+if __name__ == '__main__':
+    entry()
